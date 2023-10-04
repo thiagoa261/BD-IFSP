ccc33840ac3421299b39449b3c5b8a59d25a0d05
Thiago Aio <thiagoa371@gmail.com>
2023-10-03 22:16:41 -0300

    exercicios a fazer

--- Fabrica/triggers/exercicios/exercicios2.sql
+++ Fabrica/triggers/exercicios/exercicios2.sql
@@ -1,1 +1,18 @@
-subir depois
+1 - Faça um trigger com variáveis para, ao invés de inserir uma peça com os valores que o usuário informou, 
+não deixe o preço da peça ser inferior a 5. 
+
+2 – Crie um Trigger que ao invés de excluir os fornecedores que não são das categorias ‘A’, ‘B’ e ‘C’, 
+atualize suas categorias para ‘C’. 
+
+3 - Ao se inserir um projeto, verificar se o custo está acima do permitido (50000). Se for, incluir com 
+o limite de 50.000. Se não, incluir com o valor passado. 
+
+4 - Criar uma tabela de Historico de Projetos. 
+
+5 - Criar um trigger para atualizar a tabela Histórico de Projetos que é disparado quando 
+houver a modificação SOMENTE no atributo PDuração no Projeto. 
+
+6 - Criar 2 tabelas: Projeto_atualizado e Projeto_antigo 
+
+7 - Criar um trigger que armazena em uma tabela os dados atualizados do 
+Projeto(Projeto_atualizado) e em outra tabela os dados do Projeto antes de atualizar (Projeto_antigo) 
