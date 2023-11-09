8a8bbf4ee12644a566932c895c9f0a8efd4005ee
thiagoa261 <thiagoa371@gmail.com>
2023-11-09 17:13:13 -0300

    exercicios

--- Fabrica/transactions/exercicios/exercicios1.sql
+++ Fabrica/transactions/exercicios/exercicios1.sql
@@ -1,29 +1,38 @@
---Faça duas transações não aninhadas para atualização de custo em 10% na tabela projeto 
+--Faça duas transações não aninhadas para atualização de custo em 10% na tabela projeto
 --e inserção de um registro na tabela de fornecedores.
 BEGIN TRANSACTION Up_Custo
 	 update Projeto set PCusto = PCusto + (PCusto*10/100)
-	 
+
 	 if @@ERROR <> 0
 		ROLLBACK TRANSACTION Up_Custo
 	 else
 		COMMIT TRANSACTION Up_Custo
-		
-		
-		
-create procedure InsereForn (
-	@FNro int,
-	@FNome varchar(30),
-	@FCidade varchar(30),
-	@FCateg varchar(1)
-) as
-BEGIN
-	BEGIN TRANSACTION InserirF
-	insert into Fornecedor values(@FNro, @FNome, @FCidade, @FCateg)
-	
-	if @@ERROR <> 0
-		ROLLBACK TRANSACTION InserirF
-	else
-		COMMIT TRANSACTION InserirF
-END
---exec InsereForn 80, 'teste', 'Catanduva', 'A';
+
+
+BEGIN TRANSACTION InserirF
+insert into Fornecedor values(100, 'Nome', 'Catanduva', 'Z')
+
+if @@ERROR <> 0
+	ROLLBACK TRANSACTION InserirF
+else
+	COMMIT TRANSACTION InserirF
 --select * from Fornecedor;
+
+
+
+--Reescreva as operações do exercício anterior com duas transações aninhadas.
+BEGIN TRANSACTION Up_Custo
+	update Projeto set PCusto = PCusto + (PCusto*10/100)
+
+	if @@ERROR <> 0
+		ROLLBACK TRANSACTION Up_Custo
+	else
+		BEGIN TRANSACTION InserirF
+		insert into Fornecedor values(100, 'Nome', 'Catanduva', 'Z')
+
+		if @@ERROR <> 0
+			ROLLBACK TRANSACTION InserirF
+		else
+			COMMIT TRANSACTION InserirF
+
+		COMMIT TRANSACTION Up_Custo
